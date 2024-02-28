4a540052e5340ea75e31c21893ca7e15d25700a3
Luciana Rocha <lucianajrocha97@gmail.com>
2024-02-28 11:53:16 -0300

    alteração na model

--- farmacia-crud/src/pages/home/Home.tsx
+++ farmacia-crud/src/pages/home/Home.tsx
@@ -4,7 +4,7 @@ import './Home.css';
 function Home(){
     return (
         <>
-            <h1 className="titulo">Home</h1>
+            <h1 className="titulo">Farmácia Vila Nova</h1>
             <img src="https://i.pinimg.com/564x/45/8b/2f/458b2f2b23db77d14286e18bab80b571.jpg" alt="Imagem Tela Inicial" className="img"/>
         </>
     );
